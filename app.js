2dfbe370a64d56bd67b3d3f267277bad976d7dca
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-23 16:05:24 +0900

    2.2

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // variable 변수 사용하는 법
 
 const a = 5;
-// const b = "2"; // 2 는 text
 const b = 2;
+const veryLongVariableName = 0; // camelCase
+const very_long_variable_name = 0; // snakeCase
+const myName = "nico";
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
+console.log("hello " + myName);
