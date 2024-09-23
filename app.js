8503084ad227d68dd7f9e7628f9d769a7e86522d
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-23 17:39:11 +0900

    2.6 Object

--- app.js
+++ app.js
@@ -1,9 +1,36 @@
-const daysOfWeek = ["mon", "thu", "fri", "sat", "sun"];
+// variable로 만들기
+const playerName = "nico";
+const playerPoint = 100;
+const playerHandsome = true;
+const playerFat = "little bit";
 
-// Get Item from Array
-console.log(daysOfWeek);
-console.log(daysOfWeek[3]);
+// player.name
+// player.point 식으로 만들면 더 좋을 것 같다. (console.log처럼)
 
-// Add one more day to the Array
-daysOfWeek.push("thr");
-console.log(daysOfWeek, "push");
+// array로 만들기
+const player1 = ["nico", 100, true, "little bit"];
+
+// object로 만들기
+const player = {
+    name : "rami",
+    point : 10,
+    fat : true,
+}
+
+// 접근법 1
+console.log(player);
+console.log(player.name);
+console.log(player.fat);
+
+// 접근법 2
+console.log(player["name"], "접근법2");
+
+// objest 수정하기
+player.fat = false;
+console.log(player.fat, "수정");
+console.log(player);
+
+// object 추가하기
+player.fav = "burger";
+console.log(player.fav, "추가");
+console.log(player);
