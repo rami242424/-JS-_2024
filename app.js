6c67d7029727c76b3b4aecdd6aa31cf95217bba2
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-23 17:06:29 +0900

    2.5 push

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const mon = "mon";
-const thu = "thu";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
+const daysOfWeek = ["mon", "thu", "fri", "sat", "sun"];
 
-const daysOfWeek1 = mon + thu + fri + sat + sun;
-const daysOfWeek2 = [mon, thu, fri, sat, sun];
+// Get Item from Array
+console.log(daysOfWeek);
+console.log(daysOfWeek[3]);
 
-const nonsense = [1, 2, "hello", true, null, false, undefined, ""];
-
-console.log(daysOfWeek1); // monthufrisatsun
-console.log(daysOfWeek2); // (5) ['mon', 'thu', 'fri', 'sat', 'sun']
-console.log(nonsense);
-
+// Add one more day to the Array
+daysOfWeek.push("thr");
+console.log(daysOfWeek, "push");
