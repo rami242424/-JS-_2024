9a5ad33d5471d595505b408e564ecc8858ec5eb0
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-23 16:03:22 +0900

    variable 만들기

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-console.log(5 + 2);
-console.log(5 * 2);
-console.log(5 / 2);
+// variable 변수 사용하는 법
+
+const a = 5;
+// const b = "2"; // 2 는 text
+const b = 2;
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
