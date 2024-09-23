cf4b2d5571b7b6adf074cf982567e4070bc0539b
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-23 16:49:38 +0900

    2.4 Boolean, null, undefined

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-// const : 절대 불변
-// let 바뀔 수 있음
-let a = 5;
-let b = 2;
-let myName = "nico";
+// boolean
+const amIFat = true;
+console.log("amIFat", amIFat);
 
-console.log(a + b);
-console.log(a * b);
-console.log(a / b);
-console.log("hello " + myName); // hello nico
+// null
+// 아무 것도 없는 것으로 채워짐. (비어있다기보다 아무것도 없는 상태로 채워진 것)
+// 절대 자연적으로 생기지 않음. 우리가 variable안에 어떤 것이 없다는 것을 확실히 하기 위해 쓰는 것.
+const amIPretty = null;  
+console.log("amIPretty", amIPretty);
 
-myName = "rami";
-console.log("hello2 " + myName); // hello2 rami
+// undefined
+// variable은 존재하는데, 정의되지 않은것 (undefined)
+// 컴퓨터 메모리 안에는 존재함(공간있음), but 값이 들어가지 않은 것
+let something; 
+console.log("something", something);
