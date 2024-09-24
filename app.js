2c5d70ea1cfc1a55f4f53fa932ef722c86659ff7
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-24 14:50:22 +0900

    2.14

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
-// parseInt :  A string to convert into a number. : string -> number
-// 예시 console.log(typeof "15", typeof parseInt("15")); // string, number
-
 const age = parseInt(prompt("how old are you?"));
-console.log(age);
+
+// console.log(isNaN(age)); // 숫자 입력하면 false, 숫자가 아니면 true
+
+// // confition has to be boolean
+// if(conditon){
+//     // condition === true
+// } else {
+//     // conditon === false
+// }
+
+if(isNaN(age)){
+    // true = 숫자가 아닌것이 입력됨
+    console.log("숫자를 입력하세요.");
+} else {
+    // false = 숫자가 입력됨
+    console.log(age);
+}
+
