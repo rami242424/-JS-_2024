c429db21e8612fce4411204126fb30b33d8b2845
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-24 14:08:56 +0900

    2.11 returns

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
-// 너무 길고 많다.
-// console.log("Hello my name is rami");
-// console.log("Hello my name is borami");
-// console.log("Hello my name is hope");
-// console.log("Hello my name is ramini");
-
-// 위코드 보완
-function sayHello(){
-    console.log("Hello my name is ");
+const age = 96;
+function calculateKrAge(ageOfForeigner){ // ageOfForeigner = 96
+    return ageOfForeigner + 2; // ageOfForeigner + 2 = 98
 }
 
-// console.log(sayHello()); // Hello my name is // undefined
-// console.log(sayHello); // ƒ sayHello(){console.log("Hello my name is ");}
-sayHello(); // Hello my name is  >> !!
+const krAge = calculateKrAge(age); // calculateKrAge(96) // 98
+
+console.log(krAge); // 98
