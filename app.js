f1058acb5fab8402b2cb67a871e48dfc2250e535
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-24 16:07:02 +0900

    2.16 Recap, condition 중첩해결하기

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const age = parseInt(prompt("how old are you?"));
 
-if(isNaN(age) || age < 0){ // || 둘중에 하나만 true여도 true
-    // true = 숫자가 아닌것이 입력됨
+if(isNaN(age) || age < 0){ 
     console.log("정수로 된 숫자를 입력하세요.");
 } else if(age < 18) {
     console.log("You are too young to drink");
@@ -9,16 +8,8 @@ if(isNaN(age) || age < 0){ // || 둘중에 하나만 true여도 true
     console.log("You can drink");
 } else if(age > 50 && age <= 80) {
     console.log("take care of your healthy");
+} else if(age === 100){
+    console.log("wow you are wise");
 } else if(age > 80){
     console.log("You can do whatever you want");
 }
-
-true && true === true
-true && false === false
-false && true === false
-false && false === false
-
-true || true === true
-true || false === true
-false || true === true
-false || false === false
