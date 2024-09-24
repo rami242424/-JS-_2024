26ca2f34df468c01b8b0d4faa6a77717feec5e7e
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-24 15:56:40 +0900

    2.15

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const age = parseInt(prompt("how old are you?"));
 
-// console.log(isNaN(age)); // 숫자 입력하면 false, 숫자가 아니면 true
-
-// // confition has to be boolean
-// if(conditon){
-//     // condition === true
-// } else {
-//     // conditon === false
-// }
-
-if(isNaN(age)){
+if(isNaN(age) || age < 0){ // || 둘중에 하나만 true여도 true
     // true = 숫자가 아닌것이 입력됨
-    console.log("숫자를 입력하세요.");
-} else {
-    // false = 숫자가 입력됨
-    console.log(age);
+    console.log("정수로 된 숫자를 입력하세요.");
+} else if(age < 18) {
+    console.log("You are too young to drink");
+} else if(age >= 18 && age <= 50){
+    console.log("You can drink");
+} else if(age > 50 && age <= 80) {
+    console.log("take care of your healthy");
+} else if(age > 80){
+    console.log("You can do whatever you want");
 }
 
+true && true === true
+true && false === false
+false && true === false
+false && false === false
+
+true || true === true
+true || false === true
+false || true === true
+false || false === false
