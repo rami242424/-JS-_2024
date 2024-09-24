409eb45526548cb86cdc7dbf2efc879fe4097218
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-24 16:24:38 +0900

    #3.1 HTML in Javascript

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-document.title = "Hello, from app.js";
-//  <title>Hello, from HTML</title> 로 저장
+const title = document.getElementById("title");
 
-// 이렇게 하면 웹 사이트 title은 app.js가 된다.
-
-// 콘솔창에서 document.title = "hello"; 를 통해서도 일시적인 변경이 가능하다.
+// console.dir(title);
+title.innerText = "got ya"; // javascript로 html변경!
+console.log(title.id);
+console.log(title.className);
