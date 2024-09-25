b90ac81715cc02cabbd14ccb87df55d01618e158
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-25 16:48:46 +0900

    #3.5 More Events

--- app.js
+++ app.js
@@ -15,8 +15,33 @@ function handleMouseLeave(){
     title.innerText = "Mouse is gone";
 }
 
-title.addEventListener("click", handleTitleClick);
+// title.addEventListener("click", handleTitleClick);
+title.onclick = handleTitleClick;
 
-title.addEventListener("mouseenter", handleMouseEnter);
+// title.addEventListener("mouseenter", handleMouseEnter);
+title.onmouseenter = handleMouseEnter;
 
-title.addEventListener("mouseleave", handleMouseLeave);
+// title.addEventListener("mouseleave", handleMouseLeave);
+title.onmouseleave = handleMouseLeave;
+
+
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+window.addEventListener("resize", handleWindowResize);
+
+
+function handleWindowCopy(){
+    alert("copier!");
+}
+window.addEventListener("copy", handleWindowCopy);
+
+function handleWindowOffline(){
+    alert("S!O!S!");
+}
+window.addEventListener("offline", handleWindowOffline);
+
+function handleWindowOnline(){
+    alert("ALL GOOD");
+}
+window.addEventListener("online", handleWindowOnline);
