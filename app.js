e3787152dd1243227d98c233689405c22592ff1d
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-25 16:31:12 +0900

    #3.4 Events part Two

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 const title  = document.querySelector(".hello h1");
 
-console.log(title);
+console.dir(title);
+
+function handleTitleClick(){
+    title.style.color = "blue";
+    // console.log("Title was Clicked!");
+}
+
+function handleMouseEnter(){
+    title.innerText = "Mouse is here";
+}
+
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone";
+}
+
+title.addEventListener("click", handleTitleClick);
+
+title.addEventListener("mouseenter", handleMouseEnter);
+
+title.addEventListener("mouseleave", handleMouseLeave);
