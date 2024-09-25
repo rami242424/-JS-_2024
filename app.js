ae728a14d670d5cfee6f99619baf9c2c78b5ee2a
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-25 15:54:14 +0900

    3.2 Searching For Elements : getElementById

--- app.js
+++ app.js
@@ -1,6 +1,3 @@
-// 한번에 많은 목록을 가져와야 할떄
-// const hellos = document.getElementsByClassName("hello");
-const title = document.querySelector(".hello h1");
+const title = document.getElementById("title");
 
-// console.log(hellos); 
-console.log(title); 
+console.log(title.innerText); // 
