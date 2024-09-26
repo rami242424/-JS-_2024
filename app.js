d6feb9e2d90c382657f7a06db1754e1cb1dfe0dc
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-26 14:57:58 +0900

    3.7 CSS in JVS part two : active로 컬러바꾸기

--- app.js
+++ app.js
@@ -1,47 +1,11 @@
-const title  = document.querySelector(".hello h1");
-
-console.dir(title);
+const h1  = document.querySelector(".hello h1");
 
 function handleTitleClick(){
-    title.style.color = "blue";
-    // console.log("Title was Clicked!");
+    if(h1.className === "active"){
+        h1.className = "";
+    } else {
+        h1.className = "active";
+    }
 }
 
-function handleMouseEnter(){
-    title.innerText = "Mouse is here";
-}
-
-function handleMouseLeave(){
-    title.innerText = "Mouse is gone";
-}
-
-// title.addEventListener("click", handleTitleClick);
-title.onclick = handleTitleClick;
-
-// title.addEventListener("mouseenter", handleMouseEnter);
-title.onmouseenter = handleMouseEnter;
-
-// title.addEventListener("mouseleave", handleMouseLeave);
-title.onmouseleave = handleMouseLeave;
-
-
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-window.addEventListener("resize", handleWindowResize);
-
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-window.addEventListener("copy", handleWindowCopy);
-
-function handleWindowOffline(){
-    alert("S!O!S!");
-}
-window.addEventListener("offline", handleWindowOffline);
-
-function handleWindowOnline(){
-    alert("ALL GOOD");
-}
-window.addEventListener("online", handleWindowOnline);
+h1.onclick = handleTitleClick;
