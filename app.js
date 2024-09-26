4c1347c9f8a36826890154945953b94c6978d8ed
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-26 17:54:41 +0900

    #4.5 Saving Username

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
-
-// input이 입력되고 submit이 되면 
+ 
 function onLoginSubmit(potato){
     potato.preventDefault()
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem("username", username);
     greeting.classList.remove(HIDDEN_CLASSNAME);
-    // greeting.innerHTML = "Hello " + username;
     greeting.innerHTML = `Hello ${username}`;
 }
 
