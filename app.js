334153a90425da50dd517ab27ff703df4d18c99d
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-26 15:46:34 +0900

    4.1 Form Submission

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
-// const loginForm = document.getElementById("login-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginButton = loginForm.querySelector("button");
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
+
 function onLoginBtnClick(){
-    console.log("cilck!");
-    console.dir(loginInput.value);
+    const username = loginInput.value;
+    console.log(username);
 }
 
 loginButton.addEventListener("click", onLoginBtnClick);
