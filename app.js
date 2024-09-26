239e9b4be6c00effb451706fb12cff1ca0b51fb1
Rami <138556019+rami242424@users.noreply.github.com>
2024-09-26 17:43:22 +0900

    #4.4 Getting Username

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
+const HIDDEN_CLASSNAME = "hidden";
 
+// input이 입력되고 submit이 되면 
 function onLoginSubmit(potato){
     potato.preventDefault()
-    console.log(loginInput.value);
-    console.log(potato);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    // greeting.innerHTML = "Hello " + username;
+    greeting.innerHTML = `Hello ${username}`;
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
