a64a21143c115f452e2a3246bc1de218e140213f
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-06 23:20:46 +0900

    4.6 Loading Username

--- app.js
+++ app.js
@@ -3,14 +3,28 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
- 
+const USERNAME_KEY= "username";
+
 function onLoginSubmit(potato){
     potato.preventDefault()
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    localStorage.setItem("username", username);
+    // 입력한 이름 저장
+    localStorage.setItem(USERNAME_KEY, username);
     greeting.classList.remove(HIDDEN_CLASSNAME);
-    greeting.innerHTML = `Hello ${username}`;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+// console.log(savedUsername);
+if (savedUsername === null){
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    greeting.innerText = `Hello ${savedUsername}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
