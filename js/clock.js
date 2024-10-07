be54bab986161fa5434a321ea8edb7d5ef76a7a8
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-07 23:12:47 +0900

    #5.0 Intervals

---
+++ js/clock.js
@@ -0,0 +1,10 @@
+const clock = document.querySelector("h2#clock");
+// const clock = document.querySelector("#clock");
+// const clock = document.elementById("clock");
+
+
+function sayHello(){
+    console.log("hello");
+}
+
+setInterval(sayHello, 5000);
