22474d0bd6ac9469de148504c3eb40bb6ae38586
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-07 23:25:18 +0900

    #5.1 Timeouts and Dates

--- js/clock.js
+++ js/clock.js
@@ -3,8 +3,11 @@ const clock = document.querySelector("h2#clock");
 // const clock = document.elementById("clock");
 
 
-function sayHello(){
-    console.log("hello");
+function getClock(){
+    const date = new Date();
+    clock.innerText = (`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`);
 }
 
-setInterval(sayHello, 5000);
+
+getClock(); // 실행하면 항상 1초뒤부터 보여지기 때문에 함수를 바로 실행함으로써 갭없게만듬
+setInterval(getClock, 1000);
