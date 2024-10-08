0f393988549e7ddd2c9b4cc3e60cb78e9bdf9f7b
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-08 19:09:13 +0900

    #7.5 Loading To Dos part Two

--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,7 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY  = "todos";
-const toDos = [];
+let toDos = [];
 
 function saveToDos(){
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
@@ -53,5 +53,6 @@ console.log(savedToDos, "savedToDos");
 if(savedToDos){
     const parsedToDos = JSON.parse(savedToDos);
     // console.log(parsedToDos, "parsedToDos");
-    parsedToDos.forEach((item) => console.log("this is the turn of", item));
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
 }
