a6f862958d47177863e07f4acf7e91ce17cdfc33
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-09 16:59:31 +0900

    #3.4 Events part Two

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const title = document.getElementById("hello");
 
 function handleTitleClick(){
-    
     console.log("title was clicked!!");
     title.style.color = "blue";
 }
+
+function handleMouseEnter(){
+    title.innerText = "mouse enter";
+}
+
+function handleMouseLeave(){
+    title.innerText = "mouse leave";
+}
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
