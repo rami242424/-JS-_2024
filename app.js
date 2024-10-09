13ac8bfb1e0ba7d294d9242327d3a71a70b55b41
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-09 16:38:07 +0900

    #3.2 Searching For Elements

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-const age = parseInt(prompt("how old are you?"));
+const hellos = document.querySelector("#hello");
+// const hellos = document.getElementById("hello");
 
-// if (isNaN(age) && age < 0){ // ❌
-if (isNaN(age) || age < 0){
-    console.log("Please Write a real positive number"); // 숫자가 아니면 출력
-} else if(age < 18) { // 숫자일 경우 아래 중에 하나 출력
-    console.log("You are too young");
-} else if(age >= 18 && age <= 50) {
-    console.log("you can drink");
-} else if(age > 50 && age <= 80){
-    console.log("you should do exercise");
-} else if(age === 100){
-    console.log("WOW you are 100years-old?");
-} else if(age > 80){
-    console.log("you can do whatever you want");
-}
+
+console.log(hellos.innerText)
