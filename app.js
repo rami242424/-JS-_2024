8ed80297980002cd34111c309b509ca8db6dac2c
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-09 14:51:26 +0900

    isNaN

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
-const caluculator = {
-    plus : function (a, b){
-        return a + b;
-    },
-    minus : function (a, b){
-        return a - b;
-    },
-    divide: function (a, b){
-        return a / b;
-    },
-    multiple : function (a, b){
-        return a * b;
-    }, 
-    power : function (a, b){
-        return a ** b;
-    }
-}
+const age = parseInt(prompt("how old are you?"));
 
-const plusResult = caluculator.plus(2, 3);
-// console.log(plusResult);
-// 또는 콘솔창에 plusResult 만 입력해도 더해진 값이 출력 됨
-const minusResult = caluculator.minus(plusResult, 3);
-const divideResult = caluculator.divide(10, minusResult);
-const multipleResult = caluculator.multiple(2, divideResult);
-const powerResult = caluculator.power(multipleResult, 3);
+if (isNaN(age)){
+    console.log("write the number");
+} else {
+    console.log("thank you for writing your age");
+}
