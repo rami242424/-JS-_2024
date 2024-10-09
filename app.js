57dc5ec0ba41de4f76e8bb4fc91c6a6dccee5c1a
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-09 13:50:48 +0900

    2.8

---
+++ app.js
@@ -0,0 +1,9 @@
+const player = {
+    name: "nico",
+    sayHello: function(otherPersonsName){
+        console.log("hello " + otherPersonsName);
+    }
+}
+
+// console.log(player.name);
+player.sayHello("lynn");
