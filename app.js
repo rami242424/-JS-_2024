8050acaa918e337b5a08a5b28d6614e4a329edbe
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-09 14:05:19 +0900

    calculator 만들기

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-const player = {
-    name: "nico",
-    sayHello: function(otherPersonsName){
-        console.log("hello " + otherPersonsName);
+const caluculator = {
+    plus : function (a, b){
+        console.log(a + b);
+    },
+    minus : function (a, b){
+        console.log(a - b);
+    },
+    divide: function (a, b){
+        console.log(a / b);
+    },
+    multiple : function (a, b){
+        console.log(a * b);
+    }, 
+    power : function (a, b){
+        console.log(a ** b);
     }
 }
 
-// console.log(player.name);
-player.sayHello("lynn");
+
+caluculator.plus(1, 1); // 2
+caluculator.minus(10, 9); // 1
+caluculator.divide(10, 2); // 5
+caluculator.multiple(2, 3); // 6
+caluculator.power(2, 2); // 4
