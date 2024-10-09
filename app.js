112c35674617eebf4762c2f4c8f1b0167cb818a4
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-09 18:55:04 +0900

    #4.1 Form Submission

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-// loginForm을 정의할 필요가 있을 때,
-// const loginForm = document.querySelector("#login-form");
-// const loginForm = document.getElementById("login-form");
-// const loginInput = loginForm.querySelector("input");
-// const loginBtn = loginForm.querySelector("button");
-
-// loginForm을 정의할 필요가 없을 때,
 const loginInput = document.querySelector("#login-form input");
 const loginBtn = document.querySelector("#login-form button");
 
 function handlebtnClick(){
-    console.log(loginInput.value);
+    const username = loginInput.value;
+    console.log(username);
 }
 
 loginBtn.addEventListener("click", handlebtnClick);
