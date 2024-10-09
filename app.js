82360cda0a4b2e6db47b9d2c721a1cb6a0b2b93a
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-09 15:04:06 +0900

    2.15 && ||

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 const age = parseInt(prompt("how old are you?"));
 
-if (isNaN(age)){
-    console.log("write the number");
-} else {
-    console.log("thank you for writing your age");
+// if (isNaN(age) && age < 0){ // ❌
+if (isNaN(age) || age < 0){
+    console.log("Please Write a real positive number"); // 숫자가 아니면 출력
+} else if(age < 18) { // 숫자일 경우 아래 중에 하나 출력
+    console.log("You are too young");
+} else if(age >= 18 && age <= 50) {
+    console.log("you can drink");
+} else if(age > 50 && age <= 80){
+    console.log("you should do exercise");
+} else if(age > 80){
+    console.log("you can do whatever you want");
 }
