9a0d035963b5b4713f7e22de8782bc30c323628a
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-10 22:43:10 +0900

    #7.4 Loading To Dos part One

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,13 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+const TODOS_KEY = "todos";
+
 const toDos = [];
 
 // toDos array의 내용을 localStorage에 저장하는 함수
 function saveToDos(){
-    localStorage.setItem("todos", JSON.stringify(toDos));
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 
@@ -42,3 +44,18 @@ function handleTodoSubmit(event){
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
+
+
+
+// 로컬에 저장되어있는 todos 불러오기(string으로 저장되어있음)
+const savedToDos = localStorage.getItem(TODOS_KEY);
+// console.log(savedToDos, "before parse");
+if(savedToDos !== null){
+    // 로컬에 string으로 저장되었는 todos array로 바꾸기
+    const parsedToDos = JSON.parse(savedToDos);
+    // console.log(parsedToDos, "after parse");
+    parsedToDos.forEach((item) => console.log("hello", item));
+
+} else {
+    // 저장되있는게 없으면
+}
