9c16d2cb7ad1c9650df2a5386263f6c27a4601f7
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-10 14:45:13 +0900

    #7.1 Adding ToDos

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,21 @@ const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
 
+// todo 그리는 역할
+function paintTodo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
+
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value; 
     toDoInput.value = "";
+    paintTodo(newTodo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
