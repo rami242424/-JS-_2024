41e33aab5f512c0c12e970890261613bd04a3e4e
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-10 21:37:59 +0900

    # 7.3 Saving To Dos : JSON.stringify(toDos)를 통해 로컬에 저장되는 값들을 string으로 바꿔저장해준다

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,14 @@ const toDoForm = document.querySelector("#todo-form");
 const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.querySelector("#todo-list");
 
+const toDos = [];
+
+// toDos array의 내용을 localStorage에 저장하는 함수
+function saveToDos(){
+    localStorage.setItem("todos", JSON.stringify(toDos));
+}
+
+
 function deleteTodo(event){
     const li = event.target.parentElement;
     li.remove()
@@ -19,14 +27,16 @@ function paintToDo(newTodo){
     li.appendChild(btn);
     toDoList.appendChild(li);
     btn.addEventListener("click", deleteTodo);
-    
 }
 
 function handleTodoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
+
     
 
 }
