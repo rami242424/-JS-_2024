7ee0abe1eb2b7bead9ac4143751bccee4ef2241b
Rami <138556019+rami242424@users.noreply.github.com>
2024-10-13 21:49:33 +0900

    code edit

--- js/background.js
+++ js/background.js
@@ -1,8 +1,6 @@
 const imgs = ["0.jpg", "1.jpg", "2.jpg"];
 
-
 const chosenImg = imgs[Math.floor(Math.random() * imgs.length)];
-
 const bgimgs = document.createElement("img");
 bgimgs.src = `img/${chosenImg}`;
 document.body.appendChild(bgimgs)
